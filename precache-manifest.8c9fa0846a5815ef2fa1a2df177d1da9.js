f0db6d5c3a7618ec5c07b2875baf15a4872f7875
Lovisa Carlsson <lovisacarlssons@gmail.com>
2019-04-22 10:34:44 +0200

    Updates

---
+++ precache-manifest.8c9fa0846a5815ef2fa1a2df177d1da9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d598d569ddd5f7c5328fd0db989b389c",
+    "url": "/emmas-birthday-surprise/static/media/emma.d598d569.svg"
+  },
+  {
+    "revision": "abba7d33c86fef2644b5",
+    "url": "/emmas-birthday-surprise/static/js/runtime~main.8dcc2ad6.js"
+  },
+  {
+    "revision": "0c4c09648bf472d521f5",
+    "url": "/emmas-birthday-surprise/static/js/main.302746ce.chunk.js"
+  },
+  {
+    "revision": "ab5c9c6e34e36acc44be",
+    "url": "/emmas-birthday-surprise/static/js/2.eb8d67be.chunk.js"
+  },
+  {
+    "revision": "0c4c09648bf472d521f5",
+    "url": "/emmas-birthday-surprise/static/css/main.78f69b9a.chunk.css"
+  },
+  {
+    "revision": "28cf7f7b3ebf83e3787b36dadb3a8b0d",
+    "url": "/emmas-birthday-surprise/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/emmas-birthday-surprise/precache-manifest.8c9fa0846a5815ef2fa1a2df177d1da9.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/emmas-birthday-surprise/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
