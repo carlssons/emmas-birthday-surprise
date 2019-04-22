5f5ab0a01497065ff19ee460107fac5058ff139d
Lovisa Carlsson <lovisacarlssons@gmail.com>
2019-04-22 09:49:20 +0200

    new fixes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Modal from 'react-modal';
 
+/*const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)',
+    width: '100vw',
+    height: '100vh'
+  }
+};*/
 class App extends Component {
+  constructor() {
+    super();
+ 
+    this.state = {
+      modalIsOpen: false
+    };
+ 
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+ 
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    /*his.subtitle.style.color = '#f00';*/
+  }
+ 
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+  componentWillMount() {
+    Modal.setAppElement('body');
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="background">
+        
+          <div className="column column-1">
+            <h1>Happy B-day to Emma 28 years!</h1>
+            <button className="button sparkley" onClick={this.openModal}>Surprise<span role="img" aria-label="party-pooper"> 🎉</span></button>
+          </div>
+          
+          <div className="column column-2">
+            <div className="emma"></div>
+          </div>
       </div>
+
+      <Modal 
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          contentLabel="Example Modal"
+        >
+ 
+          <h2>Happy Happy Birthday!!</h2>
+          <iframe title="gif-frame" className="gif" src="https://giphy.com/embed/oA6sXwSYMmeyY" width="480" height="320" frameBorder="0" allowFullScreen></iframe>
+          <button className="button" onClick={this.closeModal}>close</button>
+        </Modal>
+     </div>
+
     );
   }
 }
